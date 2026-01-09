ae21ab1b1696b9f178021b221ab2ddb02e4361c9
mrayferdinand <mrayferdinand@gmail.com>
2026-01-09 13:30:44 +0700

    Add exception on support file

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // return false untuk mencegah Cypress fail test
+  return false
+})
